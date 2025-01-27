2ce22a01870ada63fdc39dc724ab0a2dba84e5fa
ismaelkennedy <ismael-kennedy.kante@ism.edu.sn>
2025-01-27 13:53:00 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div >
+      <h1 className="text-9xl">
+        Hello world!
+      </h1>
+      <Button>Tu deviens un obstacle on t'allume</Button>
+
+    </div>
+
+  )
+}
